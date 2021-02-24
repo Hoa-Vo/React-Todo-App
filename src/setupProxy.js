d6bc1227af01da0edee58413c8b98f3771cfead8
Hoa-Vo <dev.h1620@gmail.com>
2021-02-24 14:15:27 +0700

    fix route

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/api",
+    "/api/",
     createProxyMiddleware({
       target: "https://api-react-app-dev.herokuapp.com/",
       changeOrigin: true,
